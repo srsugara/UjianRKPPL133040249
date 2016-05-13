a5ed500586a9208aab9cdc4e5e328c912f09fc30
syauqirahmatsugara <syauqi.133040249@mail.unpas.ac.id>
2016-05-13 16:24:09 +0700

    nyoba log4j versi 1

---
+++ src/main/java/com/psbk/cobalog4j/App.java
@@ -0,0 +1,29 @@
+package com.psbk.cobalog4j;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger logger=Logger.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	logger.debug("Ini adalah debug message");
+        System.out.println("Debug message telah dijalankan");
+        
+        logger.info("Ini adalah info message");
+        System.out.println("Info message telah dijalankan");
+                
+        logger.fatal("Ini adalah fatal message");
+        System.out.println("Fatal message telah dijalankan");
+        
+        logger.warn("Ini adalah warn message");
+        System.out.println("Warn message telah dijalankan");
+        
+        logger.error("Ini adalah error message");
+        System.out.println("Error message telah dijalankan");
+    }
+}
